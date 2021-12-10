952bdcf35777c34396b78776599da5d580d33c97
Arman <a.arakelyan505@gmail.com>
2021-12-10 21:32:13 +0400

    first homework

---
+++ src/com/aca/Main.java
@@ -0,0 +1,39 @@
+package com.aca;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //task1
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Input youre age");
+//        System.out.println("Please Input youre weith");
+//        int myAge = scanner.nextInt();
+//        int myWaith = scanner.nextInt();
+//        int sum = myAge + myWaith;
+//        System.out.println("sum is" + sum);
+
+        //task2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Input first number");
+//        int firstNumber = scanner.nextInt();
+//        System.out.println("Please Input second number");
+//        int secondNumber = scanner.nextInt();
+//        boolean equal = firstNumber > secondNumber;
+//        System.out.println(equal);
+
+        //task3
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Input first number");
+//        int firstNumber = scanner.nextInt();
+//        System.out.println("Please Input second number");
+//        int secondNumber = scanner.nextInt();
+//        int mnacord = firstNumber % secondNumber;
+//        System.out.println("mnacord " + mnacord);
+
+        //task4
+
+    }
+}
